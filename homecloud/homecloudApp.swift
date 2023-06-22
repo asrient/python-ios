301dc8007bc4de9aa2979de237ebb0b2b1c34365
asrient <44570278+asrient@users.noreply.github.com>
2023-06-22 18:27:34 +0530

    Initial Commit

---
+++ homecloud/homecloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  homecloudApp.swift
+//  homecloud
+//
+//  Created by Aritra Sen on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct homecloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
